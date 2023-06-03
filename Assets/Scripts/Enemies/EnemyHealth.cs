cd4a514a14bf1a9194fc07f7cbb8351783c5cc19
Vasu Neppalli <vasunep0306@gmail.com>
2023-06-03 09:58:11 -0400

    Add startingHealth and currentHealth fields and Start method to EnemyHealth script

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int startingHealth = 3;
 
-    // Update is called once per frame
-    void Update()
+    private int currentHealth;
+
+    private void Start()
     {
         
     }
