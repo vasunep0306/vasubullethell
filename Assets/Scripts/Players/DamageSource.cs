a169d8159ed91eb1273b227b618d2d469dbc3900
Vasu Neppalli <vasunep0306@gmail.com>
2023-06-03 10:08:04 -0400

    Add damageAmount field and call TakeDamage method on enemy in DamageSource script

--- Assets/Scripts/Players/DamageSource.cs
+++ Assets/Scripts/Players/DamageSource.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class DamageSource : MonoBehaviour
 {
+    [SerializeField] private int damageAmount = 1;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<EnemyHealth>())
         {
-            Debug.Log("Enemy Detected");
+            EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+            enemyHealth.TakeDamage(damageAmount);
         }
     }
 }
