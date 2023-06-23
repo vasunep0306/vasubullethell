a352141972bdd3defe8f79f0e23a43a9f577287f
Vasu Neppalli <vasunep0306@gmail.com>
2023-06-23 07:05:35 -0400

    add parallax effect and leaf particles

---
+++ Assets/Scripts/Misc/Destructable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    [SerializeField] private GameObject destroyVFX;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<DamageSource>())
+        {
+            Instantiate(destroyVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
