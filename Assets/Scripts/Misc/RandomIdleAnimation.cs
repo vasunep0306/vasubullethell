a68aa404b1e78bb55988660849bf1deef89e27ef
Vasu Neppalli <vasunep0306@gmail.com>
2023-07-02 14:08:23 -0400

    push before transfer

---
+++ Assets/Scripts/Misc/RandomIdleAnimation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomIdleAnimation : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+
+    private void Start()
+    {
+        AnimatorStateInfo state = myAnimator.GetCurrentAnimatorStateInfo(0);
+        myAnimator.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+}
