62a02f6ad30f406f2476b4081282885c7e16a9fb
Vasanth Neppalli <vasunep0306@gmail.com>
2023-07-06 08:47:30 -0400

    feat: add active inventory slot functionality
    - Create ActiveInventory class to handle the active inventory slot for the player
    - Initialize and enable player controls in Awake and OnEnable methods
    - Subscribe to keyboard input event for changing the active slot in Start method
    - Define ToggleActiveSlot and ToggleActiveHighlight methods to switch the active slot and highlight it
    - Add documentation comments for each method and variable

--- Assets/Scripts/Inventory/ActiveInventory.cs
+++ Assets/Scripts/Inventory/ActiveInventory.cs
@@ -4,39 +4,46 @@ using UnityEngine;
 
 public class ActiveInventory : MonoBehaviour
 {
-    private int activeSlotIndexNum = 0;
+    /// <summary>
+    /// This class handles the active inventory slot for the player.
+    /// </summary>
+    private int activeSlotIndexNum = 0; //The index number of the active slot.
 
-    private PlayerControls playerControls;
+    private PlayerControls playerControls; //The reference to the player controls.
 
     private void Awake()
     {
-        playerControls = new PlayerControls();
+        playerControls = new PlayerControls(); //Initialize the player controls.
     }
 
     private void Start()
     {
-        playerControls.Inventory.KeyBoard.performed += ctx => ToggleActiveSlot((int) ctx.ReadValue<float>());
+        //Subscribe to the keyboard input event for changing the active slot.
+        playerControls.Inventory.KeyBoard.performed += ctx => ToggleActiveSlot((int)ctx.ReadValue<float>());
     }
 
     private void OnEnable()
     {
-        playerControls.Enable();
+        playerControls.Enable(); //Enable the player controls.
     }
 
     private void ToggleActiveSlot(int numValue)
     {
-        ToggleActiveHighlight(numValue-1);
+        //Call the method to toggle the active highlight with the input value minus one.
+        ToggleActiveHighlight(numValue - 1);
     }
 
     private void ToggleActiveHighlight(int indexNum)
     {
-        activeSlotIndexNum = indexNum;
+        activeSlotIndexNum = indexNum; //Set the active slot index number to the input value.
 
-        foreach(Transform inventorySlot in this.transform)
+        //Loop through all the inventory slots and disable their highlight game objects.
+        foreach (Transform inventorySlot in this.transform)
         {
             inventorySlot.GetChild(0).gameObject.SetActive(false);
         }
 
+        //Enable the highlight game object of the active slot.
         this.transform.GetChild(activeSlotIndexNum).GetChild(0).gameObject.SetActive(true);
     }
 }
