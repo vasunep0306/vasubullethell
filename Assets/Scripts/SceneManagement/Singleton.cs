03e2818c175b597d40ffac012009dc51fed567e1
Vasanth Neppalli <vasunep0306@gmail.com>
2023-07-06 08:56:09 -0400

    docs: add documentation and formatting to Singleton class
    - Add comments for each variable and method
    - Format the code according to C# conventions

--- Assets/Scripts/SceneManagement/Singleton.cs
+++ Assets/Scripts/SceneManagement/Singleton.cs
@@ -2,25 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton<T> : MonoBehaviour where T: Singleton<T>
+//This class implements a generic singleton pattern for MonoBehaviour classes.
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
-    private static T instance;
-    public static T Instance { get { return instance; } }
+    private static T instance; //The static instance of the singleton class.
+    public static T Instance { get { return instance; } } //The public property to access the instance.
 
+    //This method is called when the script instance is being loaded.
     protected virtual void Awake()
     {
-        if(instance != null && this.gameObject != null)
+        //If the instance already exists and this game object is not the same as the instance, destroy this game object.
+        if (instance != null && this.gameObject != null)
         {
             Destroy(this.gameObject);
-        } else
+        }
+        else
         {
+            //Otherwise, set the instance to this script.
             instance = (T)this;
         }
 
-        if(!gameObject.transform.parent)
+        //If this game object has no parent, make it persistent across scenes.
+        if (!gameObject.transform.parent)
         {
             DontDestroyOnLoad(this.gameObject);
         }
-        
+
     }
 }
