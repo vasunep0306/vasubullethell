7b0e8988e021333ccbd5a08cf5660b5244402975
Vasu Neppalli <vasunep0306@gmail.com>
2023-08-05 07:25:31 -0400

    Update IEnemy interface and Shooter class

--- Assets/Scripts/Enemies/Shooter.cs
+++ Assets/Scripts/Enemies/Shooter.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shooter : MonoBehaviour
+public class Shooter : MonoBehaviour, IEnemy
 {
+    public void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
