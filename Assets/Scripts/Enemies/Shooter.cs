bfcc0bc7194826a0a968573537043e42924bfdc3
Vasu Neppalli <vasunep0306@gmail.com>
2023-08-05 09:17:32 -0400

    Update Shooter script

--- Assets/Scripts/Enemies/Shooter.cs
+++ Assets/Scripts/Enemies/Shooter.cs
@@ -5,66 +5,121 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour, IEnemy
 {
-    // Bullet prefab to be instantiated when the enemy attacks
+    
     [SerializeField] private GameObject bulletPrefab;
-
-    // Speed at which the bullet moves
     [SerializeField] private float bulletMoveSpeed;
-
-    // Time the enemy rests after shooting a burst of bullets
     [SerializeField] private float restTime = 1f;
-
-    // Number of bullets in a burst
     [SerializeField] private int burstCount;
-
-    // Time between each bullet in a burst
     [SerializeField] private float timeBetweenBursts;
+    [SerializeField] private int projectilesPerBurst;
+    [SerializeField] [Range(0, 359)] private float angleSpread;
+    [SerializeField] private float startingDistance = 0.1f;
 
-    // Flag to indicate if the enemy is currently shooting
     private bool isShooting = false;
 
-    // Method to make the enemy attack the player
     public void Attack()
     {
-        // If the enemy is not already shooting, start the ShootRoutine coroutine
         if (!isShooting)
         {
             StartCoroutine(ShootRoutine());
         }
     }
 
-    // Coroutine to shoot a burst of bullets at the player
     private IEnumerator ShootRoutine()
     {
-        // Set isShooting to true to indicate that the enemy is currently shooting
         isShooting = true;
 
-        // Shoot a burst of bullets
+        float startAngle, currentAngle, angleStep;
+
+        TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep);
+
         for (int i = 0; i < burstCount; i++)
         {
-            // Calculate the direction from the enemy to the player
-            Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
-
-            // Instantiate a new bullet at the enemy's position
-            GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-
-            // Set the bullet's direction towards the player
-            newBullet.transform.right = targetDirection;
-
-            // If the newBullet has a Projectile component, update its move speed
-            if (newBullet.TryGetComponent(out Projectile projectile))
+            for (int j = 0; j < projectilesPerBurst; j++)
             {
-                projectile.UpdateMoveSpeed(bulletMoveSpeed);
+                Vector2 pos = FindBulletSpawnPos(currentAngle);
+
+                GameObject newBullet = Instantiate(bulletPrefab, pos, Quaternion.identity);
+                newBullet.transform.right = newBullet.transform.position - transform.position;
+
+                if (newBullet.TryGetComponent(out Projectile projectile))
+                {
+                    projectile.UpdateMoveSpeed(bulletMoveSpeed);
+                }
+
+                currentAngle += angleStep;
             }
 
-            // Wait for timeBetweenBursts before shooting the next bullet
+            currentAngle = startAngle;
+
             yield return new WaitForSeconds(timeBetweenBursts);
+            TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep);
         }
 
-        // Wait for restTime before allowing the enemy to shoot again
         yield return new WaitForSeconds(restTime);
-
-        // Set isShooting to false to indicate that the enemy has finished shooting
         isShooting = false;
     }
+
+
+    /// <summary>
+    /// Calculates the start, current, and end angles of the cone of influence for targeting the player.
+    /// </summary>
+    /// <param name="startAngle">The start angle of the cone of influence.</param>
+    /// <param name="currentAngle">The current angle of the cone of influence.</param>
+    /// <param name="angleStep">The angle step between each projectile in a burst.</param>
+    private void TargetConeOfInfluence(out float startAngle, out float currentAngle, out float angleStep)
+    {
+        // Calculate the direction from this object to the player
+        Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
+
+        // Calculate the angle from this object to the player
+        float targetAngle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
+
+        // Set the start, current, and end angles to the target angle
+        startAngle = targetAngle;
+        float endAngle = targetAngle;
+        currentAngle = targetAngle;
+
+        // Initialize halfAngleSpread and angleStep to 0
+        float halfAngleSpread = 0f;
+        angleStep = 0;
+
+        // If angleSpread is not 0
+        if (angleSpread != 0)
+        {
+            // Calculate the angle step between each projectile in a burst
+            angleStep = angleSpread / (projectilesPerBurst - 1);
+
+            // Calculate half of the angle spread
+            halfAngleSpread = angleSpread / 2f;
+
+            // Calculate the start and end angles of the cone of influence
+            startAngle = targetAngle - halfAngleSpread;
+            endAngle = targetAngle + halfAngleSpread;
+
+            // Set the current angle to the start angle
+            currentAngle = startAngle;
+        }
+    }
+
+
+    /// <summary>
+    /// Calculates the spawn position of a bullet based on the current angle.
+    /// </summary>
+    /// <param name="currentAngle">The current angle of the cone of influence.</param>
+    /// <returns>The spawn position of the bullet.</returns>
+    private Vector2 FindBulletSpawnPos(float currentAngle)
+    {
+        // Calculate the x and y coordinates of the spawn position
+        float x = transform.position.x + startingDistance * Mathf.Cos(currentAngle * Mathf.Deg2Rad);
+        float y = transform.position.y + startingDistance * Mathf.Sin(currentAngle * Mathf.Deg2Rad);
+
+        // Create a new Vector2 with the calculated coordinates
+        Vector2 pos = new Vector2(x, y);
+
+        // Return the spawn position
+        return pos;
+    }
+
+
 }
