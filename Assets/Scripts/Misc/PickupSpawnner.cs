cded5f6e41f86ef8a92c89f8377812ea58ee81be
Vasanth Neppalli <vasunep0306@gmail.com>
2023-08-20 08:33:32 -0400

    Add gold coin animation, prefab, and scripts. Modify Scene_3 and Leaf_Particle.png.meta

---
+++ Assets/Scripts/Misc/PickupSpawnner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawnner : MonoBehaviour
+{
+    [SerializeField] private GameObject goinCoinPrefab;
+
+    public void DropItems()
+    {
+        Instantiate(goinCoinPrefab, transform.position, Quaternion.identity);
+    }
+
+}
