4ae37074654192d87875cc3b2fa05c4de6dc4b60
Vasu Neppalli <vasunep0306@gmail.com>
2023-08-27 07:54:56 -0400

    modify materials

--- Assets/Scripts/Misc/PickupSpawnner.cs
+++ Assets/Scripts/Misc/PickupSpawnner.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class PickupSpawnner : MonoBehaviour
 {
-    [SerializeField] private GameObject goinCoinPrefab;
+    [SerializeField] private GameObject goldCoin, healthGlobe, staminaGlobe;
 
     public void DropItems()
     {
-        Instantiate(goinCoinPrefab, transform.position, Quaternion.identity);
+        int randomNum = Random.Range(1, 5);
+        if(randomNum == 1)
+        {
+            Instantiate(goldCoin, transform.position, Quaternion.identity);
+        }
+        if (randomNum == 2)
+        {
+            Instantiate(healthGlobe, transform.position, Quaternion.identity);
+        }
+        if (randomNum == 3)
+        {
+            Instantiate(staminaGlobe, transform.position, Quaternion.identity);
+        }
+
     }
 
 }
