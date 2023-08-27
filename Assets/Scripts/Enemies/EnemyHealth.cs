7c68e51a3e184c72340601eea54f1b97d19d8cb9
Vasu Neppalli <vasunep0306@gmail.com>
2023-08-27 10:01:12 -0400

    Add DropItems call to DetectDeath method.

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -44,7 +44,7 @@ public class EnemyHealth : MonoBehaviour
         if (currentHealth <= 0)
         {
             Instantiate(deathVFXPrefab, transform.position, Quaternion.identity);
-            // Destroy the game object
+            GetComponent<PickupSpawnner>().DropItems();
             Destroy(gameObject);
         }
     }
