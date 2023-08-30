b8c560f08f7ffbbb2e44d121426ea915ec488581
Vasanth Neppalli <vasunep0306@gmail.com>
2023-08-30 06:28:19 -0400

    Add Start method to CursorManager.vb to hide cursor

--- Assets/Scripts/Misc/CursorManager.cs
+++ Assets/Scripts/Misc/CursorManager.cs
@@ -7,7 +7,7 @@ public class CursorManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
